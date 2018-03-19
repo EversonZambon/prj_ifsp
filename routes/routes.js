84322bc6a59fa88a680c67177cb46d1c1d9df225
Everson <everson.zambon@gmail.com>
2018-03-19 04:35:28 -0300

    add angularJs and config routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,17 @@ router.get('/', function(requisicao, resposta, next) {
     resposta.render('index', {titulo: 'WeComp'});
 });
 
+router.get('/index', function(request, response, next) {
+    response.render('index', {titulo: 'WeComp'});
+});
+
+router.get('/login', function(request, response, next) {
+    response.render('login', {titulo: 'WeComp - Login'});
+});
+
+router.post('/postLogin', function(request, response, next) {
+
+	console.log("aqui");
+});
+
 module.exports = router;
