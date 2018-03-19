c3bc5cbe7c665e86a5573fea5bd399a28aedcbfe
Everton Zambon <everton.zambon@gmail.com>
2018-03-19 05:30:01 -0300

    add programming page and controller

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,6 @@ router.get('/', function(requisicao, resposta, next) {
     resposta.render('index', {titulo: 'WeComp'});
 });
 
-<<<<<<< HEAD
 router.get('/login', function(requisicao, resposta, next) {
     resposta.render('login', {titulo: 'Login | WeComp'});
 });
@@ -14,19 +13,5 @@ router.get('/programacao', function(requisicao, resposta, next) {
     resposta.render('programacao', {titulo: 'Programação | WeComp'});
 });
 
-=======
-router.get('/index', function(request, response, next) {
-    response.render('index', {titulo: 'WeComp'});
-});
-
-router.get('/login', function(request, response, next) {
-    response.render('login', {titulo: 'WeComp - Login'});
-});
-
-router.post('/postLogin', function(request, response, next) {
-
-	console.log("aqui");
-});
->>>>>>> 84322bc6a59fa88a680c67177cb46d1c1d9df225
 
 module.exports = router;
